6d7b329abee09ec76e99963356e5a2dce3fe23d7
kevinmatias120 <48968645+kevinmatias120@users.noreply.github.com>
2020-06-19 17:35:40 -0300

    Update Bienvenida.js

---
+++ Bienvenidos/Bienvenida.js
@@ -0,0 +1,31 @@
+//TEXTO JSON LINKS//
+var xhttp = new XMLHttpRequest();
+let html;
+
+xhttp.open("GET","https://my-json-server.typicode.com/kevinmatias120/linkTest/db", true);
+xhttp.send();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        /*usamos los datos del archivo json*/
+        html = JSON.parse(this.responseText);
+        /*console.log(datos);
+        console.log(datos.Solicitudes[0]);*/
+    }
+}
+
+
+function inicio(){
+    html.Redireccion[1].Inicio;
+}
+
+function misSolicitudes(){
+    html.Redireccion[1].misSolicitudes;
+}
+
+function usuarios(){
+    html.Redireccion[1].Usuarios;
+}
+
+function salir(){
+    html.Redireccion[1].Salir;
+}
